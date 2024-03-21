e732c7d54512fc558e93e7216b2f0cb0cbdcf68b
JeoungHojin <ssrosos@naver.com>
2024-03-21 14:06:13 +0900

    입력받은 두 수 중에 더 큰 수를 출력하는 프로그램 변수 a,b 만들어서 비교해서 출력

---
+++ src/Lec2Ex1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Lec2Ex1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		int a;
+		int b;
+		a = input.nextInt();
+		b = input.nextInt();
+		if (a>b) {
+			System.out.println(a);
+		}
+		else {
+			System.out.println(b);
+		}
+	}
+
+}
