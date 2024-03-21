3ba8a460871c2aa77fe701bccdd9d4a506674e6b
JeoungHojin <ssrosos@naver.com>
2024-03-21 13:42:41 +0900

    짝수, 홀수 구별하기 키보드에서 입력받은 정수를 홀수인지 짝수인지 판별합니다.

---
+++ src/EvenOdd.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class EvenOdd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		System.out.println("정수를 입력하시오");
+		int su;
+		su = input.nextInt();
+		boolean isOdd;
+		isOdd = (su % 2 ==0);
+		if (isOdd = true) {
+			System.out.println("짝수입니다.");
+		}
+		else {
+			System.out.println("홀수입니다");
+		}
+		System.out.println("프로그램을 종료합니다");
+	}
+}
