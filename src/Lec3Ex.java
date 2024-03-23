453fb3d26c3916de036bb37c7fc24cf0045c60c8
JeoungHojin <ssrosos@naver.com>
2024-03-23 23:39:48 +0900

    약수 표현

---
+++ src/Lec3Ex.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Lec3Ex {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("정수을 입력하시오 : ");
+		int number = input.nextInt();
+		
+		System.out.println("의 약수는 다음과 같습니다");
+		for(int i=1; i <= Math.sqrt(number);i++) {
+			
+		}
+	}
+}
