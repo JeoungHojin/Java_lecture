be0baf53042c3281dd39596c9bad8926045d3179
JeoungHojin <ssrosos@naver.com>
2024-03-28 11:18:54 +0900

    자음 바뀔 떄마다 줄바뀜 출력 완료

--- src/KoreanChar.java
+++ src/KoreanChar.java
@@ -1,16 +1,15 @@
-
 public class KoreanChar {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		char korean;
-		char[] ja = {'ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ'};
-		korean = '\uAC00';
-		do {
-			System.out.println(korean);
-			korean++;
-		} while (korean < '\uD7A4');
-			
-	}
+    public static void main(String[] args) {
+        char korean = '\uAC00';
 
+        do {
+            System.out.println(korean);
+            korean++;
+
+            if (korean == '\uAE4C' || korean == '\uB098' || korean == '\uB2E4' || korean == '\uB300'|| korean == '\uB77C' || korean == '\uBA54' || korean == '\uB0A0' || korean == '\uB9E4'|| korean == '\uC0AC' || korean == '\uC2B8' || korean == '\uC544' || korean == '\uC790'|| korean == '\uC9DC' || korean == '\uCC28' || korean == '\uCE74' || korean == '\uD0C0'|| korean == '\uD30C' || korean == '\uD558') {
+                System.out.println("--…--");
+            }
+        } while (korean <= '\uD7A3'); 
+    }
 }
