598e6d0e50cbf15f1906a52f8da76891fc459b50
JeoungHojin <ssrosos@naver.com>
2024-03-29 23:04:41 +0900

    객체 연습

---
+++ src/TelevsionTest.java
@@ -0,0 +1,18 @@
+
+public class TelevsionTest {
+
+	public static void main(String[] args) {
+		Television tv1 = new Television();
+		tv1.setChannel(3);
+		tv1.volume = 10;
+		tv1.onOff =true;
+		tv1.Print();
+		
+		Television tv2 = new Television();
+		tv2.setChannel(7);
+		tv2.volume = 12;
+		tv2.Print();
+		String a;
+		//String의 부모가 Object다
+	}
+}
